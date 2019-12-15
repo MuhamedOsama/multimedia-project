c07cbff7049e2135b4c9423ef294268a688a24b7
Mohammed <mido5219@gmail.com>
2019-12-15 17:29:50 +0200

    safe starting point

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-container>
+      <v-row class="justify-center text-center">
+        <v-col cols="4">
+          <h1 class="mt-6">Upload Videos!</h1>
+          <v-file-input class="mt-6" outlined @change="log" v-model="file" show-size counter small-chips label="File input w/ small chips"></v-file-input>
+          <video v-if="video" class="mt-6" autoplay controls width="520" :src="video">
+          <source type="video/mp4" :src="video">
+          </video>
+      </v-col>
+      </v-row>
+      
+      
+    </v-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "home",
+  data (){
+    return{
+      file : null,
+      video:''
+    }
+    
+  },
+  methods: {
+    log(){
+      this.video = URL.createObjectURL(this.file)
+      console.log(this.file)
+    }
   }
-}
+};
 </script>
